e59d88fe84d8f473d5fd182bb544c26def7e2620
Mikkel <92383751+Undersnyfler@users.noreply.github.com>
2025-05-07 12:01:59 +0200

    Update cd.py

--- preprocess/cd.py
+++ preprocess/cd.py
@@ -44,4 +44,4 @@ def main():
     #split_and_merge_files(files)
 
     #TODO: for testing, need to call with path to combined .npy file dynamically
-    split_files_for_evolving(f"../data/{args.dataset}/data_train_init.npy")
+    split_files_for_evolving(f"../data/{args.dataset}/train_init.npy")
